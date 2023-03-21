bbb545633d9f72f5c1374579736c0881dfa4716b
joseegabriel <joseg081@gmail.com>
2023-03-21 16:35:46 -0300

    Criando o modulo categories

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
-const client = new Client ({
+const client = new Client({
   host: 'localhost',
   port: 5432,
-  user: 'root',
-  password: 'root',
+  user: 'postgres',
+  password: 'josegabriel12',
   database: 'mycontacts',
 })
 
